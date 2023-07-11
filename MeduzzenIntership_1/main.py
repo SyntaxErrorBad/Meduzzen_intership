55efca10bc8f6814862f21e950f35e6c699bae36
Vitali <130182133+SyntaxErrorBad@users.noreply.github.com>
2023-07-11 18:55:01 +0300

    Update main.py

--- MeduzzenIntership_1/main.py
+++ MeduzzenIntership_1/main.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
-from dotenv import load_dotenv
-import os
+from config import MySettings
 
 app = FastAPI()
-#env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-load_dotenv("../venv/.env")
-
-host = os.getenv("HOST")
-port = int(os.getenv("PORT"))
-reload = os.getenv("RELOAD")
-
-
 
 @app.get("/")
 def home():
@@ -21,9 +12,14 @@ def home():
         "result" : "working"
         }
 
-@app.get("/health")
-def health():
-    return {"status" : "Ok"}
+@app.get("/health/{check}")
+def health_check(check):
+    return {
+        "status_code": check,
+        "detail": "ok",
+        "result": "working"
+    }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=host,port=port,reload=reload)
+    uvicorn.run("main:app", host=MySettings().host,port=MySettings().port,reload=MySettings().reload)
+
